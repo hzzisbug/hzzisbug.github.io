c310c477d303011ba192263d331d9bd62731199e
大概是BUG <756845494@qq.com>
2023-02-03 19:02:03 +0800

    1

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230203.184737';
+const cacheName = 'chirpy-20230203.190030';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
