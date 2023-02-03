5b4a66e657bd08b8ef791ac1520f585a20464504
大概是BUG <756845494@qq.com>
2023-02-03 19:57:21 +0800

    5

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230203.194843';
+const cacheName = 'chirpy-20230203.195658';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
